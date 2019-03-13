a37c498873b9d39ed644e5c28fb46705f25b6136
Alexandre Faustino da Silva <48439371+deadfirer@users.noreply.github.com>
2019-03-13 17:59:45 -0300

    Add files via upload

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,23 @@
 'use strict'
 
 import React from 'react';
+import ajax from '@fdaciuk/ajax';
 
 const Search = () => (
     <div className='search'>
         <input 
             type='text' 
             placeholder='Buscar usuário no Github'
+           onKeyUp = {(e) => {
+                const key = e.which || e.keyCode;
+                const ENTER = 13;
+                if (keyCode == ENTER) {
+                    ajax().get(`https://api.github.com/users/${value}`)
+                    .then((result) => {
+
+                    });
+                }
+            } }
         />
 
     </div>
