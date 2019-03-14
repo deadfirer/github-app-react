59b7c235be4385064d6d5e7f3e0d8f538dc8f593
Alexandre Faustino da Silva <48439371+deadfirer@users.noreply.github.com>
2019-03-14 18:03:20 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -14,12 +14,25 @@ class App extends React.Component{
         }
     }
 
+    getGitHubApiUrl (username , type) {
+        
+        const internalUser = username ? `/${username}` : '';
+
+        const internalType = type ? `/${type}` : '';
+
+        return `https://api.github.com/users${internalUser}${internalType}`;
+    }
+
     handleSearch (e) {
         const value = e.target.value;
         const keyCode = e.which || e.keyCode;
         const ENTER = 13;
+        const target = e.target;
+        
         if (keyCode == ENTER) {
-            ajax().get(`https://api.github.com/users/${value}`)
+            target.disabled = true;
+            this.setState({ isFetching:true });
+            ajax().get( this.getGitHubApiUrl(value) )
             .then((result) => {
                 this.setState ({
                     userinfo: {
@@ -29,19 +42,46 @@ class App extends React.Component{
                         repos: result.public_repos,
                         followers: result.followers,
                         following: result.following
-
-                    }
+                    },
+                    repos: [],
+                    starred: []
                 })
-            });
+            }).always(() =>{
+                target.disabled = false;
+                this.setState({ isFetching : false});
+            }) 
         }
     }     
+
+    getRepos(type) {
+        return (e) => {
+            
+            const username=this.state.userinfo.login;
+
+            ajax().get( this.getGitHubApiUrl(username, type) )
+            .then((result) =>{
+                this.setState({
+                    [type]: result.map((repo) => {
+                        return {
+                            name: repo.name,
+                            link: repo.html_url
+                        }
+                    })
+                });
+                
+            });
+        }
+    }
 
     render (){
         return <AppContent
             userinfo={this.state.userinfo} 
             repos={this.state.repos} 
-            starred={this.state.starred} 
+            starred={this.state.starred}
+            isFetching={this.state.isFetching} 
             handleSearch={(e) => this.handleSearch(e)}
+            getRepos={this.getRepos('repos')}
+            getStarred={this.getRepos('starred')}
         />;
     }
 }
--- src/components/app-content.js
+++ src/components/app-content.js
@@ -6,31 +6,43 @@ import UserInfo from './user-info';
 import Actions from './actions';
 import Repos from './repos';
 
-const AppContent = ({userinfo, repos, starred, handleSearch}) => (
+const AppContent = ({
+    userinfo,
+    repos,
+    starred,
+    isFetching,
+    handleSearch,
+    getRepos,
+    getStarred
+}) => (
 
-<div className='app'>
-    <Search handleSearch={handleSearch} />               
-    {!!userinfo && <UserInfo userinfo={userinfo}/>}
-    
-    {!!userinfo && <Actions />}
+    <div className='app'>
+        <Search handleSearch={handleSearch} />               
+        {isFetching && <div>Carregando...</div>}
+        {!!userinfo && <UserInfo userinfo={userinfo}/>}
+        
+        {!!userinfo && <Actions 
+            getRepos={getRepos}
+            getStarred={getStarred}
+        />}
 
-    {!!repos.length &&
-        <Repos 
-            className='repos'
-            title='Repositório' 
-            repos={repos}
-        />
-    }
+        {!!repos.length &&
+            <Repos 
+                className='repos'
+                title='Repositório' 
+                repos={repos}
+            />
+        }
 
-    {!!starred.length &&
-        <Repos 
-            className='starred'
-            title='Favoritos' 
-            repos={starred}
-        />
-    }
+        {!!starred.length &&
+            <Repos 
+                className='starred'
+                title='Favoritos' 
+                repos={starred}
+            />
+        }
 
-</div> 
+    </div> 
 
 );
 
